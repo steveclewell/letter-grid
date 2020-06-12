d3c47c0a3040bda94694acff5f281581d0861b6f
Stephen Clewell <>
2020-06-12 18:27:37 -0400

    Output Random letters

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as SeedRandom from 'seed-random';
+import Prando from 'prando'
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -7,64 +8,72 @@ import * as SeedRandom from 'seed-random';
 })
 export class GridComponent implements OnInit {
 
-  test: string = "1";
+  seed: string = "seed5";
+  rng = new Prando(this.seed);
+  board: Array<string> = new Array<string>();
 
   constructor() { }
 
   ngOnInit(): void {
-    let dice1: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice2: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice3: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice4: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice5: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice6: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice7: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice8: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice9: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice10: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice11: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice12: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice13: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice14: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice15: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice16: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice17: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice18: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice19: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice20: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice21: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice22: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice23: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice24: Array<string> = ["A", "B", "C", "D", "E", "F"];
-    let dice25: Array<string> = ["A", "B", "C", "D", "E", "F"];
+    let die1: Array<string> = ["A", "E", "D", "N", "Z", "V"];
+    let die2: Array<string> = ["O", "A", "QU", "M", "B", "J"];
+    let die3: Array<string> = ["T", "S", "L", "P", "E", "U"];
+    let die4: Array<string> = ["O", "E", "S", "W", "N", "D"];
+    let die5: Array<string> = ["N", "G", "V", "T", "I", "E"];
+    let die6: Array<string> = ["B", "X", "O", "I", "R", "F"];
+    let die7: Array<string> = ["Y", "G", "E", "U", "K", "L"];
+    let die8: Array<string> = ["A", "E", "P", "C", "D", "M"];
+    let die9: Array<string> = ["C", "T", "O", "A", "A", "I"];
+    let die10: Array<string> = ["L", "R", "C", "S", "A", "E"];
+    let die11: Array<string> = ["L", "B", "T", "Y", "I", "A"];
+    let die12: Array<string> = ["F", "H", "E", "Y", "E", "I"];
+    let die13: Array<string> = ["L", "W", "R", "G", "U", "I"];
+    let die14: Array<string> = ["S", "H", "O", "A", "R", "M"];
+    let die15: Array<string> = ["E", "I", "N", "P", "H", "S"];
+    let die16: Array<string> = ["T", "K", "U", "O", "D", "N"];
 
-    let board: Array<Array<string>> = [
-      dice1, 
-      dice2,
-      dice3, 
-      dice4,
-      dice5, 
-      dice6, 
-      dice7,
-      dice8, 
-      dice9,
-      dice10, 
-      dice11, 
-      dice12,
-      dice13, 
-      dice14,
-      dice15, 
-      dice16, 
-      dice17,
-      dice18, 
-      dice19,
-      dice20, 
-      dice21, 
-      dice22,
-      dice23, 
-      dice24,
-      dice25];
+    let dice: Array<Array<string>> = [
+      die1, 
+      die2,
+      die3, 
+      die4,
+      die5, 
+      die6, 
+      die7,
+      die8, 
+      die9,
+      die10, 
+      die11, 
+      die12,
+      die13, 
+      die14,
+      die15, 
+      die16];
 
+      let tempBoard: Array<string> = new Array<string>();
+      dice.forEach(die => {
+        let index = Math.floor(this.rng.next() * die.length);
+        tempBoard.push(die[index]);
+      });
+
+      this.board = this.shuffle(tempBoard);
   }
 
+  shuffle(array: Array<string>) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(this.rng.next() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 }
